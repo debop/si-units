aa9fec862d49fbce67b585b3e116be0f259f741a
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2016-06-17 22:35:58 +0900

    -. Storage 테스트 코드 추가

--- src/test/kotlin/com/github/debop/siunits/StorageFunSpec.kt
+++ src/test/kotlin/com/github/debop/siunits/StorageFunSpec.kt
@@ -1,5 +1,6 @@
 package com.github.debop.siunits
 
+import io.kotlintest.matchers.be
 import io.kotlintest.specs.FunSpec
 
 class StorageFunSpec : FunSpec() {
@@ -17,6 +18,43 @@ class StorageFunSpec : FunSpec() {
       100.toXBytes().inTBytes() shouldEqual 100.0 * 1024 * 1024
       100.toZBytes().inPBytes() shouldEqual 100.0 * 1024 * 1024
       100.toYBytes().inXBytes() shouldEqual 100.0 * 1024 * 1024
+    }
+
+    test("toHuman") {
+      100.toBytes().toHuman() shouldEqual "100 B"
+      123.toKBytes().toHuman() shouldEqual "123.0 KB"
+      123.43.toMBytes().toHuman() shouldEqual "123.4 MB"
+      12.59.toGBytes().toHuman() shouldEqual "12.6 GB"
+    }
+
+    test("parsing Storage String") {
+      Storage.parse("100 B") shouldEqual 100.toBytes()
+      Storage.parse("17.5 KB") shouldEqual 17.5.toKBytes()
+      Storage.parse("8.1 MB") shouldEqual 8.1.toMBytes()
+    }
+
+    test("negative") {
+      -Storage(100.0) shouldEqual -100.toBytes()
+    }
+
+    test("arithmetic - plus/minus") {
+      val a = Storage(100.0)
+      val b = Storage(200.0)
+      a + b shouldEqual 300.toBytes()
+      b - a shouldEqual 100.toBytes()
+    }
+    test("arithmetic - times/div") {
+      val a = Storage(100.0)
+      val b = Storage(200.0)
+
+      a * 2 shouldEqual b
+      b / 2 shouldEqual a
+    }
+
+    test("compare") {
+      1.7.toKBytes().compareTo(1.7.toBytes()) should be gt 0
+      1.2.toMBytes().compareTo(1.2.toKBytes()) should be gt 0
+      1.1.toMBytes().compareTo(0.9.toGBytes()) should be lt 0
     }
   }
 }
